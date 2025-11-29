835e09b0c1ab3367e98cfb93335a4d2904ceafbf
AjinkyaSawale <ajinkyasawalee@gmail.com>
2025-11-29 00:34:26 +0000

    Main.tf is updated to pin Subnet A

--- terraform/modules/webserver/main.tf
+++ terraform/modules/webserver/main.tf
@@ -1,8 +1,12 @@
 resource "aws_instance" "web" {
-  ami           = "ami-0c7217cdde317cfec"   # Amazon Linux 2 in us-east-1
+  ami           = "ami-0c7217cdde317cfec"
   instance_type = var.instance_type
+
+  # NEW: pin to Subnet A for this test
+  subnet_id = "subnet-05ee3aaa56bbfed14"
 
   tags = {
     Name = "terraform-webserver"
   }
 }
+
